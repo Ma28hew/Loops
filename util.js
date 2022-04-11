abe5144fe33cecbd284cf98587d4dc644601e4ea
Ma28hew <98984621+Ma28hew@users.noreply.github.com>
2022-04-11 13:09:39 -0600

    Add files via upload

---
+++ util.js
@@ -0,0 +1,29 @@
+let num = 1;
+while (num <= 500) {
+    console.log("I'm So Happy!");
+    num++;
+}
+
+for (let num2 = 12; num2 <= 800; num2 += 4) {
+    console.log(num2);
+}
+
+for (let num3 = 55; num3 >= 11; num3 -= 2) {
+    console.log(num3);
+}
+
+for (let num4 = 55; num4 >= 11; num4 -= 2) {
+    console.log(num4);
+}
+
+let total = 0;
+for (let n = 5; n <= 50; n++) {
+    total = total + n;
+}
+console.log(total)
+
+let total2 = 0;
+for (let n = 10; n <= 100; n++) {
+    total2 = total2 + n;
+}
+console.log(total2)
